3fbe377e4114dd575719dbbe46b4ffa3628ba263
Fuki Hayashi <knightf884@yahoo.co.jp>
2020-12-31 00:24:34 +0900

    Fix：エラーメッセージの確認を追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,28 +5,39 @@ RSpec.describe Task, type: :model do
     it 'is valid with all attributes' do
       task = FactoryBot.build(:task)
       expect(task).to be_valid
+      expect(task.errors).to be_empty
     end
+
     it 'is invalid without title' do
       task = FactoryBot.build(:task, title: nil)
       expect(task).to_not be_valid
+      expect(task.errors).to_not be_empty
     end
+
     it 'is invalid without status' do
       task = FactoryBot.build(:task, status: nil)
       expect(task).to_not be_valid
+      expect(task.errors).to_not be_empty
     end
+
     it 'is invalid with a duplicate title' do
       FactoryBot.create(:task)
       task = FactoryBot.build(:task)
       expect(task).to_not be_valid
+      expect(task.errors).to_not be_empty
     end
+
     it 'is valid with another title' do
       FactoryBot.create(:task)
       task = FactoryBot.build(:task, title: 'another title')
       expect(task).to be_valid
+      expect(task.errors).to be_empty
     end
+
     it 'is invalid without user' do
       task = FactoryBot.build(:task, user: nil)
       expect(task).to_not be_valid
+      expect(task.errors).to_not be_empty
     end
   end
 end
