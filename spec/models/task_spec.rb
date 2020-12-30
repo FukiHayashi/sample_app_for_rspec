8b5567b0640c0b85436a321ccf25e0ac40074154
Fuki Hayashi <knightf884@yahoo.co.jp>
2020-12-31 00:30:51 +0900

    Fix:Factorybotの省略と変数名の修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,41 +3,41 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'is valid with all attributes' do
-      task = FactoryBot.build(:task)
-      expect(task).to be_valid
-      expect(task.errors).to be_empty
+      task_with_all_attributes = build(:task)
+      expect(task_with_all_attributes).to be_valid
+      expect(task_with_all_attributes.errors).to be_empty
     end
 
     it 'is invalid without title' do
-      task = FactoryBot.build(:task, title: nil)
-      expect(task).to_not be_valid
-      expect(task.errors).to_not be_empty
+      task_without_title = build(:task, title: nil)
+      expect(task_without_title).to_not be_valid
+      expect(task_without_title.errors).to_not be_empty
     end
 
     it 'is invalid without status' do
-      task = FactoryBot.build(:task, status: nil)
-      expect(task).to_not be_valid
-      expect(task.errors).to_not be_empty
+      task_without_status = build(:task, status: nil)
+      expect(task_without_status).to_not be_valid
+      expect(task_without_status.errors).to_not be_empty
     end
 
     it 'is invalid with a duplicate title' do
-      FactoryBot.create(:task)
-      task = FactoryBot.build(:task)
-      expect(task).to_not be_valid
-      expect(task.errors).to_not be_empty
+      create(:task)
+      task_with_duplicate_title = build(:task)
+      expect(task_with_duplicate_title).to_not be_valid
+      expect(task_with_duplicate_title.errors).to_not be_empty
     end
 
     it 'is valid with another title' do
-      FactoryBot.create(:task)
-      task = FactoryBot.build(:task, title: 'another title')
-      expect(task).to be_valid
-      expect(task.errors).to be_empty
+      create(:task)
+      task_with_anoter_title = build(:task, title: 'another title')
+      expect(task_with_anoter_title).to be_valid
+      expect(task_with_anoter_title.errors).to be_empty
     end
 
     it 'is invalid without user' do
-      task = FactoryBot.build(:task, user: nil)
-      expect(task).to_not be_valid
-      expect(task.errors).to_not be_empty
+      task_without_user = build(:task, user: nil)
+      expect(task_without_user).to_not be_valid
+      expect(task_without_user.errors).to_not be_empty
     end
   end
 end
