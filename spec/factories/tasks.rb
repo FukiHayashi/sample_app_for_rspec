c951996e4656cd689efa1171c739ed6da922c101
Fuki Hayashi <knightf884@yahoo.co.jp>
2020-12-31 00:15:00 +0900

    taskモデルのバリデーションテスト作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    user
+    title {"title"}
+    status {:todo}
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with all attributes' do
+      task = FactoryBot.build(:task)
+      expect(task).to be_valid
+    end
+    it 'is invalid without title' do
+      task = FactoryBot.build(:task, title: nil)
+      expect(task).to_not be_valid
+    end
+    it 'is invalid without status' do
+      task = FactoryBot.build(:task, status: nil)
+      expect(task).to_not be_valid
+    end
+    it 'is invalid with a duplicate title' do
+      FactoryBot.create(:task)
+      task = FactoryBot.build(:task)
+      expect(task).to_not be_valid
+    end
+    it 'is valid with another title' do
+      FactoryBot.create(:task)
+      task = FactoryBot.build(:task, title: 'another title')
+      expect(task).to be_valid
+    end
+    it 'is invalid without user' do
+      task = FactoryBot.build(:task, user: nil)
+      expect(task).to_not be_valid
+    end
+  end
 end
